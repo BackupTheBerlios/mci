f0ff018d9cebf096f8606ddfc1579159620ac79d
delaett <delaett>
2005-03-12 17:32:16 +0000

    Initial revision

---
+++ config/features/spamfilter/reclass.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+SITE=$(hostname | cut -d"." -f2-)
+BASE="/${SITE}/me/mail"
+
+cd ${BASE}
+
+IFS="
+"
+for user in $(find * -type d -maxdepth 0 -mindepth 0)
+do
+	cd ${BASE}
+	if [ -d "${user}/Maildir/.spam/cur" ]
+	then
+		cd ${user}/Maildir/.spam/cur
+		for i in $(ls -1)
+		do
+			cat $i | /var/qmail/bin/qmail-inject ${user}-spam@${SITE}  &&
+			rm -f $i
+		done 
+	fi
+done
